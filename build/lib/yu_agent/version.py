c4b59b8c2c4fdcc5555d8fa959d1b7f804bcb1e5
xiaoyu <wuzhuoyang252@gmail.com>
2026-02-13 04:09:39 +0800

    更新项目结构

--- build/lib/yu_agent/version.py
+++
@@ -1,6 +0,0 @@
-"""版本信息"""
-
-__version__ = "0.0.1"
-__author__ = "Zhuoyang Wu"
-__email__ = "wuzhuoyang252@gmail.com"
-__description__ = "学习阶段的agent框架实现"
--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="yu_agent",
-    version="0.1.1",
-# exclude 参数可以排除掉不需要打包的文件夹
-packages=find_packages(exclude=["test", "test.*", "tests","test_the_yu_agent","test_the_yu_agent.*"]),
+    version="0.1.2",
+    # src 布局：源代码在 src 目录中
+    packages=find_packages(where="src", exclude=["tests", "tests.*"]),
+    package_dir={"": "src"},
     install_requires=[
         "google_search_results==2.4.2",
         "openai==2.18.0",
@@ -14,8 +15,8 @@ packages=find_packages(exclude=["test", "test.*", "tests","test_the_yu_agent","t
         "tavily_python==0.7.19"
     ],
     author="Zhuoyang Wu",
-    description="学习阶段的agent框架实现,主体是helloagents中的大致框架,后续会逐步完善",
+    description="学习阶段的agent框架实现,主体是helloagents中的大致框架,补充了memory和rag相关的实现",
     license="MIT",
-    url="https://github.com/xiaoyu-ops/agent",
+    url="https://github.com/xiaoyu-ops/yu_agent",
     python_requires='>=3.10',
 )
